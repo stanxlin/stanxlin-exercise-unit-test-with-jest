05cefb51d50b86bd739c64a82953517faa4b32e5
stanxlin <143833064+stanxlin@users.noreply.github.com>
2024-02-22 01:58:17 +0000

    made the tests for conversions

---
+++ app.js
@@ -0,0 +1,35 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen= function(valueInDollar) {
+    
+    let valueInYen = valueInDollar * 156.5;
+   
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+  
+    let valueInPound = valueInYen * 0.87;
+   
+    return valueInPound;
+}
+
+
+
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+module.exports = { sum, fromEuroToDollar, fromYenToPound, fromDollarToYen }
